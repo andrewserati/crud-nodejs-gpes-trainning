46d9d8283fd29ba52cb42b430921306536457cdf
SirLordX <andrewse2015@hotmail.com>
2020-02-15 14:40:50 -0300

    adicionei o arquivo database.js

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ app.use(cors());
 
 // requisição de inicio
 app.get('/', (req, res) => {
-    res.send({ message: 'FUNCIONOUUU!!!!' })
+    res.send({ message: 'Esta API foi desenvolvida como treinamento de Angular + NodeJS  correlacionado ao GPES (Grupo de Pesquisa em Engenharia de Software da Fatec Taquaritinga). Esta API contém um CRUD simples de filmes!' })
 })
 
+// requisição inicial deu algum problema
 app.use('*', (req, res) => res.send({ message: 'API não encontrada' }))
 
+// indicativo de que tá funfando belezinha!
 app.listen(PORT, () => console.log(`Aplicação rodando na porta ${PORT}!`))
 
 module.exports = app
