7fbd3731e5ed1d18a618106fc2650ffaa4b8bdcf
SirLordX <andrewse2015@hotmail.com>
2020-02-15 15:02:38 -0300

    configurei o banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 require('dotenv').config()
 
 mongoose
-    .connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`, {
         useUnifiedTopology: true,
         useNewUrlParser: true,
     })
