fcd8bab3ea90dc0b3229789873572482b7e9bafa
SirLordX <andrewse2015@hotmail.com>
2020-02-15 14:53:08 -0300

    adicionei o pacote mongoose, e adicionei o arquivo .env

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const app = express();
+const app = express(); // express config
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const PORT = process.env.PORT || process.env.APP_PORT || 3000;
+const database = require('./src/config/database') // database config
 
 // body parser config
 app.use(bodyParser.json());
@@ -14,12 +15,12 @@ app.use(bodyParser.json({ type: 'application/json' }));
 // cors config
 app.use(cors());
 
-// requisição de inicio
+// requisição inicial
 app.get('/', (req, res) => {
     res.send({ message: 'Esta API foi desenvolvida como treinamento de Angular + NodeJS  correlacionado ao GPES (Grupo de Pesquisa em Engenharia de Software da Fatec Taquaritinga). Esta API contém um CRUD simples de filmes!' })
 })
 
-// requisição inicial deu algum problema
+// indicativo de que a requisição inicial deu algum problema
 app.use('*', (req, res) => res.send({ message: 'API não encontrada' }))
 
 // indicativo de que tá funfando belezinha!
