caedaea402b33e220255ec394da4b9fda987501c
SirLordX <andrewse2015@hotmail.com>
2020-02-15 16:19:42 -0300

    fix controller filme

--- src/app/controllers/filme.js
+++ src/app/controllers/filme.js
@@ -8,7 +8,7 @@ class Filme {
             if (err) {
                 res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
             } else {
-                res.status(200).json({ message: 'Filmes recuperados com sucesso', data: contato })
+                res.status(200).json({ message: 'Filmes recuperados com sucesso', data: filme })
             }
         })
     }
@@ -18,27 +18,27 @@ class Filme {
             if (err) {
                 res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
             } else {
-                res.status(201).json({ message: 'Filme inserido com sucesso', data: contato })
+                res.status(201).json({ message: 'Filme inserido com sucesso', data: filme })
             }
         })
     }
 
     update(req, res) {
-        FilmeSchema.updateOne({ _id: req.params.id }, { $set: req.body }, (err, contato) => {
+        FilmeSchema.updateOne({ _id: req.params.id }, { $set: req.body }, (err, filme) => {
             if (err) {
                 res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
             } else {
-                res.status(200).json({ message: 'Filme atualizado com sucesso', data: contato })
+                res.status(200).json({ message: 'Filme atualizado com sucesso', data: filme })
             }
         })
     }
 
     delete(req, res) {
-        FilmeSchema.deleteOne({ _id: req.params.id }, (err, contato) => {
+        FilmeSchema.deleteOne({ _id: req.params.id }, (err, filme) => {
             if (err) {
                 res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
             } else {
-                res.status(200).json({ message: 'Filme apagado com sucesso', data: contato })
+                res.status(200).json({ message: 'Filme apagado com sucesso', data: filme })
             }
         })
     }
