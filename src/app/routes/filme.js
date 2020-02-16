fdc6848e4c5d1febba23f806f94dccb505512af6
SirLordX <andrewse2015@hotmail.com>
2020-02-15 22:34:02 -0300

    fix routes

--- src/app/routes/filme.js
+++ src/app/routes/filme.js
@@ -5,7 +5,7 @@ const FilmeController = require('./../controllers/filme')
 route.get('/filme', FilmeController.get)
 route.get('/filme/:_id', FilmeController.getById)
 route.post('/filme', FilmeController.create)
-route.put('/filme/_id', FilmeController.update)
-route.delete('/filme/_id', FilmeController.delete)
+route.put('/filme/:id', FilmeController.update)
+route.delete('/filme/:id', FilmeController.delete)
 
 module.exports = route
